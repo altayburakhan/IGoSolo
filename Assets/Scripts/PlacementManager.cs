7af8d281bea97ab2a3f849f512a52a30e58872f0
BurakhanAltay <ext.burakhan.altay@partner.rovio.com>
2024-02-23 10:56:42 +0300

    Grid Offset

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -7,13 +7,14 @@ public class PlacementManager : MonoBehaviour
     [SerializeField] private GameObject mouseIndicator, cellIndicator;
     [SerializeField] private GridManager inputManager;
     [SerializeField] private Grid grid;
-
+    [SerializeField] private Vector3 offset;
 
     private void Update()
     {
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
-        cellIndicator.transform.position = grid.GetCellCenterWorld(gridPosition);
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition) + offset;
+
     }
 }
